a2aa6c2239b67e48c9bb49167d2d4fd264826d20
NolanT96 <nolant96@gmail.com>
2017-04-19 23:38:51 -0400

    Add files via upload

--- Response.java
+++ Response.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package prj5;
 
 /**
@@ -14,8 +11,6 @@ public class Response {
     private RegionEnum region;
     private boolean heard;
     private boolean liked;
-
-
     /**
      * public constructor
      * 
@@ -26,74 +21,43 @@ public class Response {
      * @param region
      *            the region the person who made this response is from
      */
-    public Response(
-        HobbyEnum hobby,
-        MajorEnum major,
-        RegionEnum region,
-        boolean heard,
-        boolean liked) {
+    public Response(HobbyEnum hobby, MajorEnum major, RegionEnum region, boolean heard, boolean liked) {
         this.hobby = hobby;
         this.major = major;
         this.region = region;
         this.heard = heard;
         this.liked = liked;
     }
-
-
     /**
      * public get method for the hobby of this response
-     * 
      * @return the hobby of the person who submitted this response
      */
     public HobbyEnum getHobby() {
         return hobby;
     }
-
-
-    /**
-     * sets whether or not a person likes a song
-     * 
-     * @param liked
-     *            whether or not person likes song
-     */
-    public void setLiked(boolean liked) {
-        this.liked = liked;
-    }
-
-
     /**
      * public get method for the major of this response
-     * 
      * @return the enum entry for the major of this response
      */
     public MajorEnum getMajor() {
         return major;
     }
-
-
     /**
      * public get method for the region this person is from
-     * 
      * @return the region enum value for this reponse
      */
     public RegionEnum getRegion() {
         return region;
     }
-
-
     /**
      * gets if the reponder has heard the song
-     * 
      * @return true if the responder has heard the song, false otherwise
      */
     public boolean hasHeard() {
         return heard;
     }
-
-
     /**
      * returns if the responder liked the song
-     * 
      * @return true if they liked it, false otherwise
      */
     public boolean liked() {
--- Song.java
+++ Song.java
@@ -1,9 +1,7 @@
-/**
- * 
- */
 package prj5;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * @author Jonathan Alexander (JMA)
@@ -91,14 +89,202 @@ public class Song {
 
 
     /**
-     * edits the response of whether someone likes a song
+     * Tells how many responses there are for this song
      * 
-     * @param index
-     *            index of response to be edited
-     * @param liked
-     *            the edit made to response
+     * @return the number of responses there are for this song
      */
-    public void editResponse(int index, boolean liked) {
-        responses.get(index).setLiked(liked);
+    public int getNumberOfReponses() {
+        return responses.size();
+    }
+
+
+    // ---------------------------------------------------------------------
+    // COUNTERS FOR RESPONSES THAT HAVE HERD THE SONG FOR EACH OF THE CATEGORIES
+    /**
+     * Gets how many of the responses have heard the song with a certain hobby
+     * 
+     * @return the number of responses that heard the song
+     * @param hobby
+     *            of the heards being counted
+     */
+    public int getHeards(HobbyEnum hobby) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the heards
+            Response current = iterator.next();
+            if (current.hasHeard() && current.getHobby().equals(hobby)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    /**
+     * Gets how many of the responses have heard the song in a certain major
+     * 
+     * @return the number of responses that heard the song with the inputed
+     *         major
+     * @param major
+     *            to get the heards for
+     */
+    public int getHeards(MajorEnum major) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the heards
+            Response current = iterator.next();
+            if (current.hasHeard() && current.getMajor().equals(major)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    /**
+     * Gets how many of the responses have heard the song in a certain region
+     * 
+     * @return the number of responses that heard the song with the inputed
+     *         region
+     * @param region
+     *            to get the heards for
+     */
+    public int getHeards(RegionEnum region) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the heards
+            Response current = iterator.next();
+            if (current.hasHeard() && current.getRegion().equals(region)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    // -------------------------------------------------------------------------------
+    // COUNTERS FOR THE NUMBER OF RESPONSES THAT LIKE THE SONG FOR THE DIFFERENT
+    // CATEGORIES
+    /**
+     * Gets how many of the responses have liked the song with a certain hobby
+     * 
+     * @return the number of responses that liked
+     *            of the likes being counted
+     */
+    public int getLikes(HobbyEnum hobby) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the likes
+            Response current = iterator.next();
+            if (current.liked() && current.getHobby().equals(hobby)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    /**
+     * Gets how many of the responses have liked the song in a certain major
+     * 
+     * @return the number of responses that liked the song with the inputed
+     *         major
+     * @param major
+     *            to get the liked for
+     */
+    public int getLikes(MajorEnum major) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the likes
+            Response current = iterator.next();
+            if (current.liked() && current.getMajor().equals(major)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    /**
+     * Gets how many of the responses have like the song in a certain region
+     * 
+     * @return the number of responses that liked the song with the inputed
+     *         region
+     * @param region
+     *            to get the likes for
+     */
+    public int getLikes(RegionEnum region) {
+        int count = 0;
+        ListIterator<Response> iterator = responses.listIterator();
+        while (iterator.hasNext()) { // Iterates through the responses arrayList
+                                     // and counts the likes
+            Response current = iterator.next();
+            if (current.liked() && current.getRegion().equals(region)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+    // ---------------------------------------------------------------------
+    /**
+     * compares the titles of the songs for alphabetic order
+     * 
+     * @return negative, zero, positive as the argument is greater, equal, and
+     *         less
+     */
+    public int compareTitle(Song s) {
+        return title.compareToIgnoreCase(s.getTitle());
+    }
+
+
+    /**
+     * compares the artists of the songs lexicographically
+     * 
+     * @return neg, zero, positive as the argument is greater, equal, and less
+     */
+    public int compareArtist(Song s) {
+        return artist.compareToIgnoreCase(s.getArtist());
+    }
+
+
+    /**
+     * compares the years the songs were released
+     * 
+     * @return negative, zero, positive as the argument year is greater than,
+     *         equal to, and less than
+     */
+    public int compareYear(Song s) {
+        return (year - s.getYear());
+    }
+
+
+    /**
+     * Compares the genres of the songs using the ORDER in the Enum file
+     * 
+     * @return negative, zero, positive as the argument genre is before, same,
+     *         after
+     */
+    public int compareGenre(Song s) {
+        return genre.compareToIgnoreCase(s.getGenre());
+    }
+
+
+    /**
+     * @return a string containing all the information about the song
+     */
+    public String toString() {
+        StringBuilder sBuilder = new StringBuilder();
+        sBuilder.append(title);
+        sBuilder.append(" by ");
+        sBuilder.append(artist);
+        sBuilder.append(" ");
+        sBuilder.append(year);
+        sBuilder.append(" ");
+        sBuilder.append(genre);
+        return sBuilder.toString();
     }
 }
