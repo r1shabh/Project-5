3be7e59889dca5646ddf5498bb0810e10df4beaa
Jonathan Michael Alexander <jma@vt.edu>
2017-04-19 21:42:09 -0400

    Add files via upload
    Songs needs testing

--- Response.java
+++ Response.java
@@ -12,6 +12,8 @@ public class Response {
     private HobbyEnum hobby;
     private MajorEnum major;
     private RegionEnum region;
+    private boolean heard;
+    private boolean liked;
     /**
      * public constructor
      * 
@@ -22,9 +24,46 @@ public class Response {
      * @param region
      *            the region the person who made this response is from
      */
-    public Response(HobbyEnum hobby, MajorEnum major, RegionEnum region) {
+    public Response(HobbyEnum hobby, MajorEnum major, RegionEnum region, boolean heard, boolean liked) {
         this.hobby = hobby;
         this.major = major;
         this.region = region;
+        this.heard = heard;
+        this.liked = liked;
+    }
+    /**
+     * public get method for the hobby of this response
+     * @return the hobby of the person who submitted this response
+     */
+    public HobbyEnum getHobby() {
+        return hobby;
+    }
+    /**
+     * public get method for the major of this response
+     * @return the enum entry for the major of this response
+     */
+    public MajorEnum getMajor() {
+        return major;
+    }
+    /**
+     * public get method for the region this person is from
+     * @return the region enum value for this reponse
+     */
+    public RegionEnum getRegion() {
+        return region;
+    }
+    /**
+     * gets if the reponder has heard the song
+     * @return true if the responder has heard the song, false otherwise
+     */
+    public boolean hasHeard() {
+        return heard;
+    }
+    /**
+     * returns if the responder liked the song
+     * @return true if they liked it, false otherwise
+     */
+    public boolean liked() {
+        return liked;
     }
 }
---
+++ ResponseTest.java
@@ -0,0 +1,41 @@
+package MusicPreferenceVisualization;
+
+public class ResponseTest extends student.TestCase{
+    private Response testResponse;
+    /**
+     * Sets up the response object for testing
+     */
+    public void setUp() {
+        testResponse = new Response(HobbyEnum.ART, MajorEnum.COMPUTER_SCIENCE, RegionEnum.NORTHEAST, true, true);
+    }
+    /**
+     * tests the getMajor method
+     */
+    public void testGetMajor() {
+    assertEquals(MajorEnum.COMPUTER_SCIENCE, testResponse.getMajor());
+    }
+    /**
+     * tests the getHobby() method
+     */
+    public void testGetHobby() {
+        assertEquals(HobbyEnum.ART, testResponse.getHobby());
+    }
+    /**
+     * tests the getRegion() method
+     */
+    public void testGetRegion() {
+        assertEquals(RegionEnum.NORTHEAST, testResponse.getRegion());
+    }
+    /**
+     * tests the hasHeard() method
+     */
+    public void testHasHeard() {
+        assertTrue(testResponse.hasHeard());
+    }
+    /**
+     * tests the liked() method
+     */
+    public void testLiked() {
+        assertTrue(testResponse.liked());
+    }
+}
