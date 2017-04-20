40c860999abc43833baf1f3deb56917da4781d63
r1shabh <ranubis01@gmail.com>
2017-04-19 23:14:00 -0400

    Add files via upload
    created a class to read files. added methods editResponse() to song and setLiked() to response in order to deal with formatting of surveyData file

--- Response.java
+++ Response.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package MusicPreferenceVisualization;
+package prj5;
 
 /**
  * @author Jonathan Alexander (JMA)
@@ -14,6 +14,8 @@ public class Response {
     private RegionEnum region;
     private boolean heard;
     private boolean liked;
+
+
     /**
      * public constructor
      * 
@@ -24,43 +26,74 @@ public class Response {
      * @param region
      *            the region the person who made this response is from
      */
-    public Response(HobbyEnum hobby, MajorEnum major, RegionEnum region, boolean heard, boolean liked) {
+    public Response(
+        HobbyEnum hobby,
+        MajorEnum major,
+        RegionEnum region,
+        boolean heard,
+        boolean liked) {
         this.hobby = hobby;
         this.major = major;
         this.region = region;
         this.heard = heard;
         this.liked = liked;
     }
+
+
     /**
      * public get method for the hobby of this response
+     * 
      * @return the hobby of the person who submitted this response
      */
     public HobbyEnum getHobby() {
         return hobby;
     }
+
+
+    /**
+     * sets whether or not a person likes a song
+     * 
+     * @param liked
+     *            whether or not person likes song
+     */
+    public void setLiked(boolean liked) {
+        this.liked = liked;
+    }
+
+
     /**
      * public get method for the major of this response
+     * 
      * @return the enum entry for the major of this response
      */
     public MajorEnum getMajor() {
         return major;
     }
+
+
     /**
      * public get method for the region this person is from
+     * 
      * @return the region enum value for this reponse
      */
     public RegionEnum getRegion() {
         return region;
     }
+
+
     /**
      * gets if the reponder has heard the song
+     * 
      * @return true if the responder has heard the song, false otherwise
      */
     public boolean hasHeard() {
         return heard;
     }
+
+
     /**
      * returns if the responder liked the song
+     * 
      * @return true if they liked it, false otherwise
      */
     public boolean liked() {
