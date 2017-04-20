182c0e1e39b64ddb35ce10b48677faf2793204da
NolanT96 <nolant96@gmail.com>
2017-04-19 23:50:19 -0400

    Add files via upload

--- SongList.java
+++ SongList.java
@@ -1,9 +1,8 @@
-/**
- * 
- */
-package MusicPreferenceVisualization;
+package prj5;
 
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
+import list.ListInterface;
 
 /**
  * @author Jonathan Alexander (jma)
@@ -20,8 +19,10 @@ public class SongList {
 
 
     public SongList() {
-        head = null; // By default head is null as there are no nodes
-        tail = null; // By default tail is null as there are no nodes
+        head = new Node();
+        tail = new Node();
+        //head = null; // By default head is null as there are no nodes
+        //tail = null; // By default tail is null as there are no nodes
         length = 0; // By default the length is 0 as there are no nodes
     }
 
@@ -33,10 +34,21 @@ public class SongList {
      *            the entry to be added to the list
      */
     public void add(Song newEntry) {
-        Node newLast = new Node(tail, null, newEntry);
-        tail.setNext(newLast);
-        tail = newLast;
-        length++;
+        if (length == 0) {
+            Node newNode = new Node(head, tail, newEntry);
+            head.setNext(newNode);
+            tail.setPrevious(newNode);
+            length++;
+        }
+        else {
+            Node end = new Node(tail.getPrevious(), tail, newEntry);
+            tail.getPrevious().setNext(end);
+            tail.setPrevious(end);
+            //Node newLast = new Node(tail, null, newEntry);
+            //tail.setNext(newLast);
+            //tail = newLast;
+            length++;
+        }
     }
 
 
@@ -46,8 +58,10 @@ public class SongList {
      * 
      */
     public void clear() {
-        head = null; // By default head is null as there are no nodes
-        tail = null; // By default tail is null as there are no nodes
+        head.setNext(null);
+        tail.setPrevious(null);
+        //head = null; // By default head is null as there are no nodes
+        //tail = null; // By default tail is null as there are no nodes
         length = 0; // By default the length is 0 as there are no nodes
     }
 
@@ -60,7 +74,7 @@ public class SongList {
      * @return the element at the given position
      */
     public Song getEntry(int givenPosition) {
-        Node current = head;
+        Node current = head.getNext();
         for (int i = 1; i < givenPosition; i++) {
             current = current.getNext();
         }
@@ -95,9 +109,9 @@ public class SongList {
      */
     public Object[] toArray() {
         Object[] toReturn = new Object[length];
-        Node current = head;
-        for (int i = 0; i < length - 1; i++) {
-            toReturn[i] = current;
+        Node current = head.getNext();
+        for (int i = 0; i <= length - 1; i++) {
+            toReturn[i] = current.getData();
             current = current.getNext();
         }
         return toReturn;
@@ -112,28 +126,26 @@ public class SongList {
     public SongIterator iterator() {
         return new SongIterator();
     }
-
-
+    
     /**
      * sorts the song list by artist using selection sort
      * 
      */
     public void sortByArtist() {
-        for (Node start = head; start != tail; start = start.getNext()) {
-            Node largest = start;
-            for (Node curr = start; curr != null; curr = curr.getNext()) {
-                if (largest.getData().compareArtist(curr.getData()) < 0) {
-                    largest = curr;
-                }
-            }
-            Song tmp = start.getData();
-            start.setData(largest.getData());
-            largest.setData(tmp);
-
-        }
+       for(Node start = head; start != tail; start = start.getNext()) {
+           Node largest = start;
+           for(Node curr = start; curr != null; curr = curr.getNext()) {
+               if(largest.getData().compareArtist(curr.getData()) < 0) {
+                   largest = curr;
+               }
+           }
+           Song tmp = start.getData();
+           start.setData(largest.getData());
+           largest.setData(tmp);
+           
+       }
     }
-
-
+    
     /**
      * sorts the song list by Genre using selection sort
      * 
@@ -205,8 +217,11 @@ public class SongList {
         Song data;
         Node next;
         Node prev;
-
-
+        
+        private Node() {
+            this(null, null, null);
+        }
+        
         private Node(Node before, Node after, Song song) {
             this.data = song;
             this.next = after;
@@ -237,20 +252,15 @@ public class SongList {
         private Song getData() {
             return data;
         }
-
-
+        
         private void setData(Song d) {
             data = d;
         }
 
 
         private void remove() {
-            if (prev != null) {
-                prev.setNext(next);
-            }
-            if (next != null) {
-                next.setPrevious(prev);
-            }
+            prev.setNext(next);
+            next.setPrevious(prev);
         }
 
     }
@@ -291,6 +301,17 @@ public class SongList {
             Node toReturn = next;
             next = next.getNext();
             return toReturn;
+        }
+
+
+        /**
+         * gets the node after the iterator without moving it
+         * 
+         * @return the node after the iterator which will be returned on the
+         *         next next() call;
+         */
+        private Node getNext() {
+            return next;
         }
 
     }
---
+++ SongListTest.java
@@ -0,0 +1,68 @@
+package prj5;
+
+import java.util.Arrays;
+
+/**
+ * tests the methods for song list
+ * @author nolant
+ * @version 04.19.2017
+ */
+public class SongListTest extends student.TestCase{
+
+    private SongList sl1;
+    private SongList sl2;
+    private Song song1;
+    private Song song2;
+    
+    /**
+     * sets up methods for testing
+     */
+    public void setUp() {
+        song1 = new Song("Boy named Sue", "Johnny Cash", 1969, "Country");
+        song2 = new Song("Same Drugs", "Chance the Rapper", 2016, "Rap");
+        sl1 = new SongList();
+        sl2 = new SongList();
+        sl1.add(song1);
+        sl1.add(song2);
+        sl2.add(song2);
+        sl2.add(song1);
+    }
+    
+    /**
+     * tests the clear method
+     */
+    public void testClear() {
+        assertEquals(2, sl1.getLength());
+        sl1.clear();
+        assertEquals(0, sl1.getLength());
+        sl1.add(song1);
+        assertEquals(1, sl1.getLength());
+        sl1.add(song2);
+        assertEquals(2, sl1.getLength());
+        assertFalse(sl1.isEmpty());
+        sl1.clear();
+        assertEquals(0, sl1.getLength());
+        assertTrue(sl1.isEmpty());
+    }
+    
+    /**
+     * tests the get entry method
+     */
+    public void testGetEntry() {
+        assertEquals(song1, sl1.getEntry(1));
+        assertEquals(song2, sl1.getEntry(2));
+    }
+    
+    /**
+     * tests the toArray method
+     */
+    public void testToArray() {
+        Object[] song = new Object[2];
+        song[0] = song1;
+        song[1] = song2;
+        Object[] sl1Array = sl1.toArray();
+        
+        assertTrue(Arrays.equals(song, sl1Array));
+    }
+
+}
