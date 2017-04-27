6cca5b9b1f1e26bf1374c0f9023d6a922fbd28ef
NolanT96 <nolant96@gmail.com>
2017-04-26 20:31:10 -0400

    Add files via upload

--- Input.java
+++ Input.java
@@ -1,7 +1,13 @@
 package prj5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+import prj5.SongList.SongIterator;
+
 /**
  * used for intermediate submission to test back end
+ * 
  * @author nolant
  * @version 04/19/2017
  *
@@ -10,14 +16,28 @@ public class Input {
 
     /**
      * main method
-     * @param args are the name of survey file 
-     * and response file
+     * 
+     * @param args
+     *            are the name of survey file
+     *            and response file
      */
-    public static void main(String[] args) {
-        if (args.length > 1) {
-            //args[0] is Song list input
-            //args[1] is Response list input
+    public static void main(String[] args) throws FileNotFoundException {
+        String songFile = "SongList2017S.csv";
+        String surveyFile = "MusicSurveyData2017S.csv";
+        if (args.length > 0) {
+            if(args[0] != null) {
+                surveyFile = args[0];
+            }
+            if(args[1] != null) {
+                songFile = args[1];
+            }
         }
+        DataReader songFileReader = new DataReader(songFile);
+        SongList list = songFileReader.readSongs();
+        list.readResponses(surveyFile);
+        new Display(list);
     }
+
+
 
 }
