ebe00fda2a4e7bc189f597c0ee1116a5b0bd2170
Jonathan Michael Alexander <jma@vt.edu>
2017-04-27 02:46:13 -0400

    Add files via upload

--- SongListTest.java
+++ SongListTest.java
@@ -1,33 +1,58 @@
 package prj5;
 
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import prj5.SongList.SongIterator;
 
 /**
  * tests the methods for song list
+ * 
  * @author nolant
  * @version 04.19.2017
  */
-public class SongListTest extends student.TestCase{
+public class SongListTest extends student.TestCase {
 
     private SongList sl1;
     private SongList sl2;
+    private SongList emptySongList;
     private Song song1;
     private Song song2;
-    
+    private Song song3;
+
+
     /**
      * sets up methods for testing
      */
     public void setUp() {
         song1 = new Song("Boy named Sue", "Johnny Cash", 1969, "Country");
         song2 = new Song("Same Drugs", "Chance the Rapper", 2016, "Rap");
+        song3 = new Song("All of the Lights", "Kanye West", 2010, "Rap");
         sl1 = new SongList();
         sl2 = new SongList();
+        emptySongList = new SongList();
         sl1.add(song1);
         sl1.add(song2);
         sl2.add(song2);
         sl2.add(song1);
+        sl2.add(song3);
     }
-    
+
+
+    /**
+     * testing the add method
+     */
+    public void testAdd() {
+        assertTrue(emptySongList.isEmpty());
+        emptySongList.add(song3);
+        assertFalse(emptySongList.isEmpty());
+        assertEquals(song3, emptySongList.getEntry(0));
+        assertEquals(1, emptySongList.getLength());
+        emptySongList.add(song2);
+        assertEquals(2, emptySongList.getLength());
+        assertEquals(song2, emptySongList.getEntry(1));
+    }
+
+
     /**
      * tests the clear method
      */
@@ -44,7 +69,8 @@ public class SongListTest extends student.TestCase{
         assertEquals(0, sl1.getLength());
         assertTrue(sl1.isEmpty());
     }
-    
+
+
     /**
      * tests the get entry method
      */
@@ -52,15 +78,8 @@ public class SongListTest extends student.TestCase{
         assertEquals(song1, sl1.getEntry(0));
         assertEquals(song2, sl1.getEntry(1));
     }
-    
-    /**
-     * tests the set entry method
-     */
-    public void testSetEntry() {
-        sl1.setEntry(0, song2);
-        assertEquals(song2, sl1.getEntry(0));
-    }
-    
+
+
     /**
      * tests the toArray method
      */
@@ -69,21 +88,132 @@ public class SongListTest extends student.TestCase{
         song[0] = song1;
         song[1] = song2;
         Object[] sl1Array = sl1.toArray();
-        
+
         assertTrue(Arrays.equals(song, sl1Array));
     }
-    
+
+
     /**
-     * tests the sort by artist
+     * testing the getLength method
+     */
+    public void testGetLength() {
+        assertEquals(0, emptySongList.getLength());
+        assertEquals(2, sl1.getLength());
+        assertEquals(3, sl2.getLength());
+
+    }
+
+
+    /**
+     * testing the is empty method for the two
+     * possible cases
+     */
+    public void testIsEmpty() {
+        assertTrue(emptySongList.isEmpty());
+        assertFalse(sl1.isEmpty());
+        assertFalse(sl2.isEmpty());
+
+    }
+
+
+    /**
+     * testing the iterator() method
+     */
+    public void testIterator() {
+        assertNotNull(sl1.iterator());
+        SongIterator iter = sl1.iterator();
+        assertEquals(song1, iter.next());
+        assertEquals(song2, iter.next());
+
+    }
+
+
+    /**
+     * testing the sort by artist method
      */
     public void testSortByArtist() {
-        Object[] sl1array = sl1.toArray();
-        Object[] sl2array = sl2.toArray();
-        assertFalse(Arrays.equals(sl1array, sl2array));
-        sl1.sortByArtist();
-        sl1array = sl1.toArray();
-        sl2array = sl2.toArray();
-        assertTrue(Arrays.equals(sl1array, sl2array));
+        sl2.add(song3);
+        sl2.sortByArtist();
+        assertEquals(song2, sl2.getEntry(0));
+        assertEquals(song1, sl2.getEntry(1));
+        assertEquals(song3, sl2.getEntry(2));
+        assertEquals(song3, sl2.getEntry(3));
+        assertEquals(4, sl2.getLength());
+        assertFalse(sl2.isEmpty());
+
+    }
+
+
+    /**
+     * testing the sort by genre method
+     */
+    public void testSortByGenre() {
+        sl2.sortByGenre();
+        assertEquals(song1, sl2.getEntry(0));
+        assertEquals(song2, sl2.getEntry(1));
+        assertEquals(song3, sl2.getEntry(2));
+        assertEquals(3, sl2.getLength());
+    }
+
+
+    /**
+     * test the sort by year method
+     */
+    public void testSortByYear() {
+        sl2.sortByYear();
+        assertEquals(song1, sl2.getEntry(0));
+        assertEquals(song3, sl2.getEntry(1));
+        assertEquals(song2, sl2.getEntry(2));
+        assertEquals(3, sl2.getLength());
+    }
+
+
+    /**
+     * test the sort by title method
+     */
+    public void testSortByTitle() {
+        sl2.sortByTitle();
+        assertEquals(song3, sl2.getEntry(0));
+        assertEquals(song1, sl2.getEntry(1));
+        assertEquals(song2, sl2.getEntry(2));
+        assertEquals(3, sl2.getLength());
+    }
+
+
+    /**
+     * testing the file reading
+     * of the response reader method in the
+     * songList
+     * For this to work the songList will need to
+     * be correctly read in by the DataReader class
+     * so watch that
+     * 
+     * @throws FileNotFoundException
+     *             if the songList file cannot be found
+     */
+    public void testReadResponses() throws FileNotFoundException {
+        SongList data = new DataReader("SongList2017S.csv").readSongs();
+        
+        String nonExistentFile =
+            "There is no way I should be a file";
+        Exception exception = null;
+        try {
+            data.readResponses(nonExistentFile); // Just to make sure the method
+                                                 // correctly throws exception
+
+        }
+        catch (FileNotFoundException e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        data.readResponses("MusicSurveyData2017S.csv"); // The songs in the list
+                                                        // should now be updated
+                                                        // with responses
+        assertEquals(226, data.getEntry(0).getNumberOfReponses());
+        assertEquals(154, data.getEntry(0).getHeards());
+        
+        
+        
     }
 
 }
